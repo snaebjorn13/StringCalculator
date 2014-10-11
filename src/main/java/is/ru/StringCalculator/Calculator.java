d93d342bfe56857fbcc86738de958d1ca7b28de2
Snaebjorn <snaebjorn13@ru.is>
2014-10-11 18:35:47 +0000

    Added test for unkown number of arguments/numbers

--- src/main/java/is/ru/StringCalculator/Calculator.java
+++ src/main/java/is/ru/StringCalculator/Calculator.java
@@ -4,19 +4,14 @@ public class Calculator {
 
 	public static int add(String text){
 		 if(text.equals("")){
-		 	return 0;
-		 }
-		 else if(text.contains(",")){
-		 	String[] numbers = split(text);
-		 	int sum = 0;
-		 	for(int i = 0; i < numbers.length; i++){
-		 		sum += toInt(numbers[i]);
-		 	}
-		 	return sum;
-		 }
-		 else{
-		 	return toInt(text);
-		 }
+			return 0;
+		}
+		else if(text.contains(",")){
+			return sumOfMultipleNumbers(text);
+		}
+		else{
+			return toInt(text);
+		}
 	}
 
 	private static int toInt(String number){
@@ -25,6 +20,15 @@ public class Calculator {
 
 	private static String[] split(String text){
 		return text.split(",");
+	}
+
+	private static int sumOfMultipleNumbers(String text){
+		String[] numbers = split(text);
+		int sum = 0;
+		for(int i = 0; i < numbers.length; i++){
+			sum += toInt(numbers[i]);
+		}
+		return sum;
 	}
 
 }
