c80b1d8f70af7720c48dd0728b012778fee6af89
Snaebjorn <snaebjorn13@ru.is>
2014-10-11 17:26:18 +0000

    Added test for two numbers

--- src/main/java/is/ru/StringCalculator/Calculator.java
+++ src/main/java/is/ru/StringCalculator/Calculator.java
@@ -3,9 +3,15 @@ package is.ru.StringCalculator;
 public class Calculator {
 
 	public static int add(String text){
-		 if(text.equals(""))
+		 if(text.equals("")){
 		 	return 0;
-		 else
+		 }
+		 else if(text.contains(",")){
+		 	String[] numbers = text.split(",");
+		 	return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+		 }
+		 else{
 		 	return 1;
+		 }
 	}
 }
